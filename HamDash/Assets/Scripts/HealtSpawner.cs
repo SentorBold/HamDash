079def118c6e43fd9ac153d3b9cfdb35d6604c95
Rakun665 <yemir.yilmaz2@gmail.com>
2021-08-01 20:39:48 +0300

    healcode

---
+++ HamDash/Assets/Scripts/HealtSpawner.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealtSpawner : MonoBehaviour
+{
+
+
+    public GameObject heal;
+    Vector2 bottomLeft;
+    Vector2 BottomRight;
+    float timeCounter = 0f;
+    float spawnheal = 20f;
+
+    float x;
+    float y;
+
+   
+
+    void Start()
+    {
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        BottomRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+       
+
+        Healt();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime;
+        if (timeCounter >= spawnheal)
+        {
+            spawnheal += 20;
+            Healt();
+
+
+
+
+
+
+        }
+    }
+     void Healt()
+    {
+        x = Random.Range(bottomLeft.x, BottomRight.x);
+        y  = Random.Range(bottomLeft.y, BottomRight.y);
+        GameObject newHealt = Instantiate(heal);
+        newHealt.transform.position = new Vector2(x, y);
+    }
+}
--- HamDash/Assets/Scripts/Timer.cs
+++ HamDash/Assets/Scripts/Timer.cs
@@ -21,6 +21,7 @@ public class Timer : MonoBehaviour
            
             StageController.instance.InstantiateStage();
 
+     
 
 
         }
